98dfc53afc9e05e091ba9ceb3e794bc201d33822
jortiz00 <javydreamercsw@users.noreply.github.com>
2011-09-02 14:18:39 -0500

    Fixing compilation warnings

--- Marauroa Application Manager/Application Monitor/src/simple/marauroa/application/monitor/MonitorRepository.java
+++ Marauroa Application Manager/Application Monitor/src/simple/marauroa/application/monitor/MonitorRepository.java
@@ -1,31 +1,31 @@
 package simple.marauroa.application.monitor;
 
 import java.util.Collection;
-import org.openide.util.LookupEvent;
-import simple.marauroa.application.core.MonitorService;
 import java.util.HashMap;
 import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
 import org.openide.util.Utilities;
 import org.openide.util.lookup.ServiceProvider;
 import simple.marauroa.application.api.IApplicationMonitor;
 import simple.marauroa.application.api.IMarauroaApplication;
+import simple.marauroa.application.core.MonitorService;
 
 /**
  *
  * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
  */
 @ServiceProvider(service = MonitorService.class)
-public class MonitorRepository implements MonitorService ,LookupListener{
+public class MonitorRepository implements MonitorService, LookupListener {
 
     private HashMap<String, IApplicationMonitor> monitors =
             new HashMap<String, IApplicationMonitor>();
-    private Lookup.Result result = null;
+    private Lookup.Result<IMarauroaApplication> result = null;
     private IMarauroaApplication currentApplication;
 
     public MonitorRepository() {
         result = Utilities.actionsGlobalContext().lookupResult(IMarauroaApplication.class);
-        result.addLookupListener(this);
+        result.addLookupListener(MonitorRepository.this);
     }
 
     @Override
@@ -35,7 +35,7 @@ public class MonitorRepository implements MonitorService ,LookupListener{
 
     @Override
     public void addMonitor(String name) throws Exception {
-        MonitorClient client= new MonitorClient(getCurrentApplication());
+        MonitorClient client = new MonitorClient(getCurrentApplication());
         if (monitors.containsKey(name)) {
             throw new Exception("There's a monitor defined already for: " + name);
         } else {
@@ -56,8 +56,10 @@ public class MonitorRepository implements MonitorService ,LookupListener{
 
     @Override
     public void resultChanged(LookupEvent le) {
-        Lookup.Result r = (Lookup.Result) le.getSource();
-        Collection c = r.allInstances();
+        Lookup.Result<MonitorRepository> r =
+                (Lookup.Result<MonitorRepository>) le.getSource();
+        Collection<MonitorRepository> c = 
+                (Collection<MonitorRepository>) r.allInstances();
         //Update the selected application
         if (!c.isEmpty()) {
             setApplication((IMarauroaApplication) c.iterator().next());
