b9712784197f49bb2f04805106896566dc415665
Javier A. Ortiz Bultr?n <javydreamercsw@hg.java.net>
2011-09-29 09:40:20 -0500

    Make Player list sorted

--- Simple Marauroa Client/User List Component/src/simple/marauroa/client/components/userlist/ClientRPObjectChildFactory.java
+++ Simple Marauroa Client/User List Component/src/simple/marauroa/client/components/userlist/ClientRPObjectChildFactory.java
@@ -1,6 +1,8 @@
 package simple.marauroa.client.components.userlist;
 
 import java.beans.IntrospectionException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import marauroa.common.game.RPObject;
@@ -15,12 +17,22 @@ import simple.marauroa.application.core.EventBus;
  */
 public class ClientRPObjectChildFactory extends ChildFactory<RPObject> {
 
+    static final Comparator<RPObject> NAME_ORDER =
+            new Comparator<RPObject>() {
+
+                @Override
+                public int compare(RPObject e1, RPObject e2) {
+                    return e1.get("name").compareTo(e2.get("name"));
+                }
+            };
+
     @Override
     protected boolean createKeys(List<RPObject> list) {
-        for (Iterator<? extends RPObject> it = 
+        for (Iterator<? extends RPObject> it =
                 EventBus.getDefault().lookupAll(RPObject.class).iterator(); it.hasNext();) {
             RPObject object = it.next();
             list.add(object);
+            Collections.sort(list, NAME_ORDER);
         }
         return true;
     }
