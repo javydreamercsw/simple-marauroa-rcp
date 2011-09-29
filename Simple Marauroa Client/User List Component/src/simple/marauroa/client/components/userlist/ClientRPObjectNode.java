179f7dddce2b268e99ed8b57f4c1d85ae9fb37f4
Javier A. Ortiz Bultr?n <javydreamercsw@users.noreply.github.com>
2011-09-29 09:41:17 -0500

    Make RPObject node use Icons from plug-ins.

--- Simple Marauroa Client/User List Component/src/simple/marauroa/client/components/userlist/ClientRPObjectNode.java
+++ Simple Marauroa Client/User List Component/src/simple/marauroa/client/components/userlist/ClientRPObjectNode.java
@@ -3,13 +3,16 @@ package simple.marauroa.client.components.userlist;
 import java.awt.Image;
 import java.beans.IntrospectionException;
 import java.net.MalformedURLException;
+import java.util.Iterator;
 import javax.swing.Action;
 import marauroa.common.game.RPObject;
 import org.openide.nodes.BeanNode;
 import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 import org.openide.util.lookup.Lookups;
 import simple.marauroa.application.core.tool.Tool;
 import simple.marauroa.client.components.api.IUserListActionProvider;
+import simple.marauroa.client.components.api.IUserListIconProvider;
 
 /**
  *
@@ -24,17 +27,32 @@ public class ClientRPObjectNode extends BeanNode {
 
     @Override
     public Image getIcon(int type) {
+        Image icon = null;
         try {
-            return Tool.createImage(
-                    "simple.marauroa.application.gui",
-                    "resources/images/entity.png",
-                    "Entity icon");
+            //Check for modifiers in the Lookup
+            for (Iterator<? extends IUserListIconProvider> it = Lookup.getDefault().lookupAll(IUserListIconProvider.class).iterator(); it.hasNext();) {
+                IUserListIconProvider provider = it.next();
+                Image newIcon = provider.getIcon(getLookup().lookup(RPObject.class));
+                //Right now it takes the first non-null return. 
+                //Makes no sense to have multiple providers.
+                if (newIcon != null) {
+                    icon = newIcon;
+                    break;
+                }
+            }
+            if (icon == null) {
+                //Use default
+                icon = Tool.createImage(
+                        "simple.marauroa.application.gui",
+                        "resources/images/entity.png",
+                        "Entity icon");
+            }
         } catch (MalformedURLException ex) {
             Exceptions.printStackTrace(ex);
         } catch (Exception ex) {
             Exceptions.printStackTrace(ex);
         }
-        return null;
+        return icon;
     }
 
     @Override
