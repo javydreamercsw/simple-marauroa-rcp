1949bbf746ad96a93afd7ec3f093e0add65ed8ac
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-03-01 12:13:07 -0600

    Update to 7.1.1

--- Marauroa Application Manager/Marauroa Application Core/src/simple/marauroa/application/core/EventBusListener.java
+++ Marauroa Application Manager/Marauroa Application Core/src/simple/marauroa/application/core/EventBusListener.java
@@ -4,6 +4,7 @@ package simple.marauroa.application.core;
  * This code is taken from OpenBlueSky project. For some reason I'm unable to 
  * retrieve the original files so I ended up copying code from:
  * http://netbeans.dzone.com/news/publish-subscribe-netbeans-pla?page=0,1
+ * @param <T> 
  * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
  */
 public interface EventBusListener<T> {
