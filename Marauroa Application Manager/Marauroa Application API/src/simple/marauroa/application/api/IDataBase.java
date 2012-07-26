28ec8021aff4a800513b19af484ef711c3c8fdc8
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-07-26 15:51:44 -0500

    Add target to allow uploading from Jenkins to Sourceforge the daily build's update center

--- Marauroa Application Manager/Marauroa Application API/src/simple/marauroa/application/api/IDataBase.java
+++ Marauroa Application Manager/Marauroa Application API/src/simple/marauroa/application/api/IDataBase.java
@@ -7,15 +7,18 @@ import java.util.Collection;
  * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
  */
 public interface IDataBase {
+
     /**
      * Check if application exists
+     *
      * @param name Application name
      * @return true if it exists, false otherwise.
      */
     public boolean applicationExists(String name);
-    
+
     /**
      * Delete application from database
+     *
      * @param name Application name
      * @return true if it was removed, false otherwise.
      */
@@ -23,19 +26,22 @@ public interface IDataBase {
 
     /**
      * Adds IMarauroaApplication to database
-     * @param newInstance 
+     *
+     * @param newInstance
      */
     public void addApplication(IMarauroaApplication newInstance);
-    
+
     /**
      * Get an IMarauroaApplication from a name
+     *
      * @param name Application name
      * @return IMarauroaApplication
      */
     public IMarauroaApplication getApplication(String name);
-    
+
     /**
      * Get the Marauroa applications
+     *
      * @return List of applications
      */
     public Collection<? extends IMarauroaApplication> getIMarauroaApplications();
