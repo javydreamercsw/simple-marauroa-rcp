c1b2409c649a78ee6f093a1d88787ba49cbf9a04
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2012-09-19 11:33:59 -0500

    Adding ignored pom.xml files.

--- Marauroa-Server-Manager/Marauroa-Application-DB/src/test/java/simple/marauroa/application/core/db/manager/DataBaseManagerTest.java
+++ Marauroa-Server-Manager/Marauroa-Application-DB/src/test/java/simple/marauroa/application/core/db/manager/DataBaseManagerTest.java
@@ -1,9 +1,6 @@
 package simple.marauroa.application.core.db.manager;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openide.util.Exceptions;
@@ -30,18 +27,6 @@ public class DataBaseManagerTest {
         for (ApplicationType appType : DataBaseManager.getApplicationTypes()) {
             DataBaseManager.deleteApplicationType(appType);
         }
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
     }
 
     /**
